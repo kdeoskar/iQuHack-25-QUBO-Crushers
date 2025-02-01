7212c6bafa3e8967758ea7979eb0b50a795b70c1
Elijah Haga <HagaElijah04@gmail.com>
2025-02-01 18:40:45 -0500

    Added a lot more but it's not working, going to test with removing time dependence

--- optimization_cqm.py
+++ optimization_cqm.py
@@ -1,10 +1,12 @@
 from dimod import BinaryQuadraticModel, ConstrainedQuadraticModel, Binary, Integer
 import random
 from dwave.system import LeapHybridCQMSampler
+from dwave.optimization import Model
 
 cqm = ConstrainedQuadraticModel()
 # bqm = BinaryQuadraticModel()
 sampler = LeapHybridCQMSampler()
+model = Model()
 
 ### Given ###
 num_locations = 10
@@ -18,7 +20,7 @@ X = [[[Binary(f"{i}_{m}_{t}") for i in range(num_coolers)] for m in range(num_se
 # Server position matrix over time (should be fixed)
 Y = [[[Binary(f"{j}_{n}_{t}") for j in range(num_servers)] for n in range(num_servers+num_coolers)] for t in range(0,12)]
 # Amount of cooling provided by cooler i to server j across time
-Z = [[[Integer(f"{i}_{j}_{t}") for i in range(num_coolers)] for j in range(num_servers)] for t in range(0,12)]
+Z = [[[Binary(f"{i}_{j}_{t}") for i in range(num_coolers)] for j in range(num_servers)] for t in range(0,12)]
 # Change c and r later, they should be inputted by the user
 amount_cooling_used = [[Integer(f"{i}_{t}") for i in range(num_coolers)] for t in range(0,12)]
 amount_cooling_needed = [[Integer(f"{j}_{t}") for j in range(num_servers)] for t in range(0,12)]
@@ -26,14 +28,16 @@ cost_to_move = [[Integer(f"{i}_{t}") for i in range(num_coolers)] for t in range
 cost_to_stay = [[Integer(f"{i}_{t}") for i in range(num_coolers) for t in range(0,12)]]
 
 for j in range(num_servers):
-    cqm.add_constraint(sum([[Z[i][j][t] for i in range(num_coolers)] for t in range(0,12)]) == sum([amount_cooling_needed[j][t] for t in range(0,12)]))
+    # print(sum([amount_cooling_needed[t][j] for t in range(0,12)]))
+    # print(sum([sum([Z[t][j][i].quadratic.values()+Z[t][j][i].linear.values() for i in range(num_coolers) if type(Z[t][j][i].quadratic.values()) == int and type(Z[t][j][i].linear.values()) == int]) for t in range(0,12)]))
+    cqm.add_constraint(sum([sum([Z[t][j][i] for i in range(num_coolers)]) for t in range(0,12)]) == sum([amount_cooling_needed[t][j] for t in range(0,12)]))
 for i in range(num_coolers):
-    cqm.add_constraint(sum([[Z[i][j][t] for j in range(num_servers)] for t in range(0,12)]) == sum([amount_cooling_used[i][t] for t in range(0,12)]))
-cqm.add_constraint(sum([[[X[i][m][t] for i in range(num_coolers)] for m in range(0, num_coolers+num_servers-1)] for t in range(0,12)]) == num_coolers)
-cqm.add_constraint(sum([[[Y[j][n][t] for j in range(num_servers)] for n in range(0, num_coolers+num_servers-1)] for t in range(0,12)]) == num_servers)
+    cqm.add_constraint(sum([sum([Z[t][j][i] for j in range(num_servers)]) for t in range(0,12)]) == sum([amount_cooling_used[t][i] for t in range(0,12)]))
+cqm.add_constraint(sum([[[X[t][m][i] for i in range(num_coolers)] for m in range(0, num_coolers+num_servers)] for t in range(0,12)]) == num_coolers)
+cqm.add_constraint(sum([[[Y[t][n][j] for j in range(num_servers)] for n in range(0, num_coolers+num_servers)] for t in range(0,12)]) == num_servers)
 
 
-obj = sum([[cost_to_move[i][t] + cost_to_stay[i][t] for i in range(num_coolers)] for t in range(0,12)])
+obj = sum([[cost_to_move[t][i] + cost_to_stay[t][i] for i in range(num_coolers)] for t in range(0,12)])
 
 res = sampler.sample_cqm(cqm, time_limit=10.0)
 res.resolve()
