b68f6c89e760f97dade4a2221dd104c6344d3187
Elijah Haga <HagaElijah04@gmail.com>
2025-02-02 00:00:39 -0500

    Implemented setup_cqm in qpsolver

--- backend/qpsolver.py
+++ backend/qpsolver.py
@@ -5,42 +5,67 @@ import numpy as np
 
 class QPSolver:
     def __init__(self, A) -> None:
-        C = 5
-        D = 15
-        T = 4
+        self.C = 5
+        self.D = 15
+        self.T = 4
 
-        self.cqm = ConstrainedQuadraticModel()
         self.x = [
-            [[Binary(f"x_{i}_{m}_{t}") for t in range(T)] for m in range(C + D)]
-            for i in range(C)
+            [[Binary(f"x_{i}_{m}_{t}") for t in range(self.T)] for m in range(self.C + self.D)]
+            for i in range(self.C)
         ]
         self.y = [
-            [[Binary(f"y_{j}_{n}_{t}") for t in range(T)] for n in range(C + D)]
-            for j in range(D)
+            [[Binary(f"y_{j}_{n}_{t}") for t in range(self.T)] for n in range(self.C + self.D)]
+            for j in range(self.D)
         ]
 
         self.yz = [
             [
                 [
-                    [Binary(f"yz_{i}_{j}_{n}_{t}") for t in range(T)]
-                    for n in range(C + D)
+                    [Binary(f"yz_{i}_{j}_{n}_{t}") for t in range(self.T)]
+                    for n in range(self.C + self.D)
                 ]
-                for j in range(D)
+                for j in range(self.D)
             ]
-            for i in range(C)
+            for i in range(self.C)
         ]
 
         self.d = [
             [
                 (((j % 4) - (i % 4)) ** 2 + ((j // 4) - (i // 4)) ** 2) ** 0.5
-                for i in range(C + D)
+                for i in range(self.C + self.D)
             ]
-            for j in range(C + D)
+            for j in range(self.C + self.D)
         ]
 
     def setup_cqm(self):
-        # Setup the constraints and the objective function for the CQM
-        pass
+        # Setup the constraints and the objective function for the self.CQM
+        self.cqm = ConstrainedQuadraticModel()
+
+        for t in range(self.T):
+            self.cqm.add_constraint(
+                quicksum([self.x[i][m][t] for i in range(self.C) for m in range(self.C + self.D)]) == self.C
+            )
+
+            self.cqm.add_constraint(
+                quicksum([self.y[j][n][t] for j in range(self.D) for n in range(self.C + self.D)]) == self.D
+            )
+
+        for n in range(self.C + self.D):
+            self.cqm.add_constraint(
+                quicksum([self.x[i][n][t] for i in range(self.C)] + [self.y[j][n][t] for j in range(self.D)])
+                == 1
+            )
+
+        for j in range(self.D):
+            self.cqm.add_constraint(quicksum([self.y[j][n][t] for n in range(self.C + self.D)]) == 1)
+
+        for i in range(self.C):
+            self.cqm.add_constraint(quicksum([self.x[i][m][t] for m in range(self.C + self.D)]) == 1)
+            for j in range(self.D):
+                for n in range(self.C + self.D):
+                    self.cqm.add_constraint(self.yz[i][j][n][t] - self.y[j][n][t] <= 0)
+                    self.cqm.add_constraint(self.yz[i][j][n][t] - self.z[j][i][t] <= 0)
+                    self.cqm.add_constraint(self.yz[i][j][n][t] - self.y[j][n][t] - self.z[j][i][t] >= -1)
 
     def run_cqm(self):
         # Find an optimal solution to the quadratic program
