5de176b5f3f7dd1ceb172d62c120396a1ad0732c
Keshav Deoskar <kdeoskar@berkeley.edu>
2025-02-02 03:48:02 -0500

    upload files working. now need to write code to parse the data

--- backend/CQM/views.py
+++ backend/CQM/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import fileUpload
 
 def index(request):
     # return HttpResponse("This is the index page.")
+    if request.method == "POST":
+        fileSave = request.FILES.get("file")
+        fileUpload.objects.create(file=fileSave)
     return render(request, "index.html")
 
 # Create your views here.
